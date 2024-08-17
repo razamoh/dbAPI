5a3076929e77d94eacab4cc6b79b3a06d8f1f431
Raza <razamoh@outlook.com>
2024-08-17 22:57:18 +0100

    init commit

---
+++ app/Models/API/Transaction.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\API\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    use HasFactory;
+}
